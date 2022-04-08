5e64a28327d2b583a799f428cdaf00797d041073
LeeroyKiplagat <leeroykiplagat@gmail.com>
2022-04-08 20:29:40 +0300

    In Progress

--- components/mypages/About.js
+++ components/mypages/About.js
@@ -7,7 +7,7 @@ function About() {
         <h1 className=" text-4xl font-bold mt-24 text-red-600">About</h1>
         <h1 className="text-4xl font-bold mt-24 ">Me</h1>
       </div>
-      <div className="flex items-center justify-center"> 
+      <div className="flex items-center justify-center">
         <div className="grid md:grid-cols-2">
           <div>
             {/**Image */}
@@ -20,14 +20,14 @@ function About() {
               <div className=" w-[250px] md:w-[400px] lg:mr-[350px] border  rounded-xl shadow-xl y-[5px ] text-black bg-gray-200">
                 <div className="text-center font-semibold flex items-center justify-center space-x-2">
                   <h1>Hello, I am</h1>
-                  {""} <h1 className="text-red-600">Leeroy</h1>
+                  <h1 className="text-red-600">Leeroy</h1>
                 </div>
                 <div>
                   <h2 className="p-4 w-[250px] md:w-[400px] h-auto text-xs md:text-sm">
-                    I'm a software developer that loves building products and
-                    web applications that impact millions of lives.I've been
+                    I&apos;m a software developer that loves building products and
+                    web applications that impact millions of lives.I&apos;ve been
                     developing full-stack application for more than 3yrs. After
-                    learning HTML and struggling with CSS, I came up with a
+                    learning HTML and struggling with CSS I came up with a
                     brilliant idea of using utilising bootstrap so as not to
                     style everything by myself and for obvious reasons if you
                     knew bootstrap, you were cool, but now am knowledgeable in
