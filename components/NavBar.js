47e4ee15f8f915fc0b22bc2c3b578ab08bcf84d8
LeeroyKiplagat <leeroykiplagat@gmail.com>
2022-04-10 00:24:05 +0300

    Final

--- components/NavBar.js
+++ components/NavBar.js
@@ -11,20 +11,16 @@ import { useRouter } from "next/router";
 import NavBarItem from "./NavBarItem";
 
 function NavBar() {
-  
   return (
     <nav className="flex items-center justify-center h-auto fixed z-10 w-screen  bg-white shadow  shadow-gray-300">
-      <div className="grid md:grid-cols-2">
-        <div className=" flex items-center justify-center">
+      <div className="grid md:grid-cols-3">
+        <div className="md:ml-[300px] flex items-center justify-center ">
           <h1 className=" font-bold uppercase text-red-600 text-lg md:text-2xl lg:text-4xl">
             Leeroy
           </h1>
         </div>
-        <div className="flex flex-grow items-center justify-evenly max-w-2xl mt-4 ">
-          <button>
-            <NavBarItem title="home" Icon={HomeIcon} url="#home" />
-          </button>
-      
+        <div className="md:ml-[450px] flex md:justify-center lg:justify-between  max-w-2xl mt-4  ">
+          <NavBarItem title="home" Icon={HomeIcon} url="#home" />
 
           <NavBarItem title="about" Icon={UserIcon} url="#about" />
           <NavBarItem title="services" Icon={ServerIcon} url="#services" />
